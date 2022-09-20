63cf9ec5143458d8dd8065d512435edb01f874ae
mashkovtsevasf <103369052+mashkovtsevasf@users.noreply.github.com>
2022-09-20 20:41:54 +0300

    work

--- js/dates.js
+++ js/dates.js
@@ -23,6 +23,7 @@ function getDay(date, lang) {
       6: 'Суббота',
     },
   };
+  return dayNames.lang[date.getDay()]
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
